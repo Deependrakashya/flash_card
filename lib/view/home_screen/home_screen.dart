7779fbc83a94a1438d6fb954db6b8e2249c6002c
deependrakashya <dipendrakashyap493@gmail.com>
2024-11-17 10:10:28 +0530

    ui bug fixed

--- lib/view/home_screen/home_screen.dart
+++ lib/view/home_screen/home_screen.dart
@@ -15,128 +15,138 @@ class MainScreen extends StatefulWidget {
   State<MainScreen> createState() => _MainScreenState();
 }
 
-class _MainScreenState extends State<MainScreen>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _animation;
-  bool _isFront = true;
+class CardState {
+  late AnimationController controller;
+  late Animation<double> animation;
+  bool isFront = true;
+
+  CardState(TickerProvider vsync) {
+    controller = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: vsync,
+    );
+    animation = Tween<double>(begin: 0, end: 1).animate(controller);
+  }
+
+  void dispose() {
+    controller.dispose();
+  }
+
+  void flipCard() {
+    if (isFront) {
+      controller.forward();
+    } else {
+      controller.reverse();
+    }
+    isFront = !isFront;
+  }
+}
+
+class _MainScreenState extends State<MainScreen> with TickerProviderStateMixin {
   Controller controller = Get.put(Controller());
+  List<CardState>? cardStates;
 
   @override
   void initState() {
-    controller.fetchList();
     super.initState();
-    _controller = AnimationController(
-      duration: const Duration(seconds: 1),
-      vsync: this,
-    );
-    _animation = Tween<double>(begin: 0, end: 1).animate(_controller);
+    controller.fetchList();
   }
 
   @override
   void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  void flipCard() {
-    if (_isFront) {
-      _controller.forward();
-    } else {
-      _controller.reverse();
+    if (cardStates != null) {
+      for (var state in cardStates!) {
+        state.dispose();
+      }
     }
-    _isFront = !_isFront;
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.lightGray,
+      appBar: AppBar(
         backgroundColor: AppColors.lightGray,
-        appBar: AppBar(
-          backgroundColor: AppColors.lightGray,
-          bottom: PreferredSize(
-            preferredSize: const Size.fromHeight(0.5),
-            child: Container(
-              color: AppColors.defaultBorderColor,
-              height: 0.5,
-            ),
-          ),
-          title: Text(
-            Constants.appName,
-            style: TextStyle(fontWeight: FontWeight.bold),
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(0.5),
+          child: Container(
+            color: AppColors.defaultBorderColor,
+            height: 0.5,
           ),
         ),
-        body: Obx(() {
-          if (controller.flashCardList.isEmpty ||
-              controller.flashCardList.length == 0) {
-            return Center(child: Text('Please add a new Card'));
-          }
-          return ListView.builder(
-              itemCount: controller.flashCardList.length,
-              itemBuilder: (context, index) {
-                return Column(
-                  children: [
-                    Center(
-                      child: GestureDetector(
-                        onTap: flipCard,
-                        child: AnimatedBuilder(
-                          animation: _animation,
-                          builder: (context, child) {
-                            // Flip the card halfway
-                            final angle = _animation.value * pi;
-                            final isFrontVisible = angle < pi / 2;
+        title: Text(
+          Constants.appName,
+          style: const TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Obx(() {
+        if (controller.flashCardList.isEmpty) {
+          return const Center(child: Text('Please add a new Card'));
+        }
 
-                            return Transform(
-                              transform: Matrix4.identity()
-                                ..setEntry(3, 2, 0.001) // Perspective
-                                ..rotateY(angle),
+        // Dynamically initialize card states if they haven't been initialized yet
+        if (cardStates == null ||
+            cardStates!.length != controller.flashCardList.length) {
+          cardStates = List.generate(
+            controller.flashCardList.length,
+            (_) => CardState(this),
+          );
+        }
+
+        return ListView.builder(
+          itemCount: controller.flashCardList.length,
+          itemBuilder: (context, index) {
+            final cardState = cardStates![index];
+            final flashCard = controller.flashCardList[index];
+
+            return Center(
+              child: GestureDetector(
+                onTap: cardState.flipCard,
+                child: AnimatedBuilder(
+                  animation: cardState.animation,
+                  builder: (context, child) {
+                    final angle = cardState.animation.value * pi;
+                    final isFrontVisible = angle < pi / 2;
+
+                    return Transform(
+                      transform: Matrix4.identity()
+                        ..setEntry(3, 2, 0.001) // Perspective
+                        ..rotateY(angle),
+                      alignment: Alignment.center,
+                      child: isFrontVisible
+                          ? flashCardItem(
+                              title: flashCard.query.toString(),
+                              context: context,
+                              controller: controller,
+                              index: flashCard.id!.toInt(),
+                              cardTitle: 'Question',
+                              ans: flashCard.ans.toString(),
+                              question: flashCard.query.toString(),
+                            )
+                          : Transform(
+                              transform: Matrix4.identity()..rotateY(pi),
                               alignment: Alignment.center,
-                              child: isFrontVisible
-                                  ? flashCardItem(
-                                      title: controller
-                                          .flashCardList[index].query
-                                          .toString(),
-                                      context: context,
-                                      controller: controller,
-                                      index: controller.flashCardList[index].id!
-                                          .toInt(),
-                                      cardTitle: 'Question',
-                                      ans: controller.flashCardList[index].ans
-                                          .toString(),
-                                      question: controller
-                                          .flashCardList[index].query
-                                          .toString())
-                                  : Transform(
-                                      transform: Matrix4.identity()
-                                        ..rotateY(pi),
-                                      alignment: Alignment.center,
-                                      child: flashCardItem(
-                                        title: controller
-                                            .flashCardList[index].ans
-                                            .toString(),
-                                        context: context,
-                                        controller: controller,
-                                        index: controller
-                                            .flashCardList[index].id!
-                                            .toInt(),
-                                        cardTitle: 'Answer',
-                                        ans: controller.flashCardList[index].ans
-                                            .toString(),
-                                        question: controller
-                                            .flashCardList[index].query
-                                            .toString(),
-                                      ),
-                                    ),
-                            );
-                          },
-                        ),
-                      ),
-                    )
-                  ],
-                );
-              });
-        }),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-        floatingActionButton: addQnAButton(context, controller));
+                              child: flashCardItem(
+                                title: flashCard.ans.toString(),
+                                context: context,
+                                controller: controller,
+                                index: flashCard.id!.toInt(),
+                                cardTitle: 'Answer',
+                                ans: flashCard.ans.toString(),
+                                question: flashCard.query.toString(),
+                              ),
+                            ),
+                    );
+                  },
+                ),
+              ),
+            );
+          },
+        );
+      }),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: addQnAButton(context, controller),
+    );
   }
 }
